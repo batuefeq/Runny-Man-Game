2c79535512f9ad49eed99227e5f2b1dfe3cbaca8
Batuhan Efe Arıalp <batuhanarialp@hotmail.com>
2023-07-01 13:05:02 +0300

    mobile integration

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -44,8 +44,26 @@ public class PlayerHandler : MonoBehaviour
         }
         else // if not mobile device!
         {
+            
             x_input = Input.GetAxis("Horizontal"); // interpolating through keyboard to make it smooth.(why getaxis?)
             controller.Move(new Vector3(x_input, 0, 0) * Time.deltaTime * playerSpeed);
+            print("yes this is");
+            foreach (Touch touch in Input.touches)  // take touches
+            {
+                if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) // if touch just began
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position); // took the ray whether do smthn !
+                    if (touch.position.x <= Screen.width / 2)
+                    {
+                        controller.Move(Vector3.left * Time.deltaTime * playerSpeed); // if left side of the screen move to left
+                    }
+                    else
+                    {
+                        controller.Move(Vector3.right * Time.deltaTime * playerSpeed); // otherwise move to right !
+                    }
+
+                }
+            }
         }     
     }
 }
