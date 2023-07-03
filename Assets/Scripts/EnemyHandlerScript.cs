350e042c6e6b4b1e1d05ca3237417605a5754da6
Batuhan Efe Arıalp <batuhanarialp@hotmail.com>
2023-07-03 21:00:36 +0300

    Yorma

---
+++ Assets/Scripts/EnemyHandlerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandlerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemy, obstacle;
+
+
+    private void Awake()
+    {
+        StartCoroutine(GameobjectSpawner());
+    }
+
+
+    private IEnumerator GameobjectSpawner()
+    {
+        print("inside");
+        Instantiate(enemy);
+        yield return new WaitForSeconds(3f);
+        print("outside");
+    }
+  
+    void Update()
+    {        
+    }
+}
--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TouchPhase = UnityEngine.TouchPhase;
 
 public class PlayerHandler : MonoBehaviour
 {
@@ -7,6 +9,8 @@ public class PlayerHandler : MonoBehaviour
 
     [SerializeField]
     private float playerSpeed;
+
+
 
     private void Awake()
     {
@@ -20,50 +24,38 @@ public class PlayerHandler : MonoBehaviour
     }
 
 
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        float inputMovement = value.ReadValue<Vector2>().x;
+        x_input = inputMovement;
+        print(x_input);
+    }
+
+
     private void InputAndMovementHandle()
     {
-        if (DeviceTypeHandler.TypeFinder()) // on mobile device!
-        {
-            print("yes this is");
-            foreach (Touch touch in Input.touches)  // take touches
-            {
-                if (touch.phase == TouchPhase.Began) // if touch just began
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position); // took the ray whether do smthn !
-                    if (touch.position.x <= Screen.width / 2)
-                    {
-                        controller.Move(Vector3.left * Time.deltaTime * playerSpeed); // if left side of the screen move to left
-                    }
-                    else
-                    {
-                        controller.Move(Vector3.right * Time.deltaTime * playerSpeed); // otherwise move to right !
-                    }
 
-                }             
-            }          
-        }
-        else // if not mobile device!
+        foreach (Touch touch in Input.touches)  // take touches
         {
-            
-            x_input = Input.GetAxis("Horizontal"); // interpolating through keyboard to make it smooth.(why getaxis?)
-            controller.Move(new Vector3(x_input, 0, 0) * Time.deltaTime * playerSpeed);
-            print("yes this is");
-            foreach (Touch touch in Input.touches)  // take touches
+            if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) // if touch just began
             {
-                if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) // if touch just began
+                Ray ray = Camera.main.ScreenPointToRay(touch.position); // took the ray and maybe do smthn !
+                if (touch.position.x <= Screen.width / 2)
                 {
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position); // took the ray whether do smthn !
-                    if (touch.position.x <= Screen.width / 2)
-                    {
-                        controller.Move(Vector3.left * Time.deltaTime * playerSpeed); // if left side of the screen move to left
-                    }
-                    else
-                    {
-                        controller.Move(Vector3.right * Time.deltaTime * playerSpeed); // otherwise move to right !
-                    }
-
+                    controller.Move(Vector3.left * Time.deltaTime * playerSpeed); // if left side of the screen move to left
                 }
+                else
+                {
+                    controller.Move(Vector3.right * Time.deltaTime * playerSpeed); // otherwise move to right !
+                }
+
             }
-        }     
+        }
+        controller.Move(new Vector3(x_input, 0, 0) * Time.deltaTime * playerSpeed);
     }
+
+
+
+
+
 }
